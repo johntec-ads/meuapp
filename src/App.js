345906de4b1848a6ed174d1a95e8a0f4d3c1e652
johntec-ads <johntec.ads@gmail.com>
2024-02-12 21:08:08 -0300

    upCursoReact

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-export default function app (){
+import React from "react";
+
+/* A criação de componentes deve ser efetuada com uma 
+aero function, seguindo a mesma sintaxe do JS, sendo 
+reduzida quando o código estiver em apenas uma linha, do contrario
+deve ser afetuada com (arg ou props) + (=>) + ({chaves}) .
+Exemplo:
+No caso, a variável do componente, de ser com letra
+Maiúscula.
+
+const Variável = (props) => {
+  return (
+    Aqui deve ser usado o return com chaves
+  )
+}  */
+
+const Atletas = (props) => {
   return (
     <div>
-      <h1>Welcome to system</h1>
-      <h2>johntec.ads@gmail.com</h2>
+      <h3> {props.nome} </h3>
+      <h3> {props.posicao} </h3>
+      <h3> {props.habilidade} </h3>
     </div>
+
   )
 }
+
+
+
+function app() {
+  return (
+    <div>
+      <h1>Bem vindo</h1>
+      <Atletas nome = 'Jogador: John'/>      
+      <Atletas posicao = 'Posição: Meio'/>      
+      <Atletas habilidade = 'Habilidade: 3' />      
+      Olá Mundo!
+    </div>
+  );
+};
+
+export default app
---
+++ src/indexTreino.js
@@ -0,0 +1,12 @@
+
+
+
+function app () {
+  return(
+    <div>
+      <h1>Conheça nossa equipe</h1>
+
+
+    </div>
+  )
+}
