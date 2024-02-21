1d6b554797cc9ea35fccba0e81d58e7100a07110
johntec-ads <johntec.ads@gmail.com>
2024-02-20 22:45:49 -0300

    class_Component

--- src/App.js
+++ src/App.js
@@ -10,48 +10,50 @@ const Variável = (props) => {
     Aqui deve ser usado o return com chaves
   )
 }  */
-import React from "react"
-const Equipe = (props) => {
-  return (
-    <div>
-      <Sobre nome={props.nome} cargo={props.cargo}
-        idade={props.idade} />
-      <Social goo={props.google} face= {props.facebook} />      
-    <hr/>
-    </div>
-  )
-}
 
+import React, { Component } from "react";
 
-
-const Sobre = (props) => {
-  return (
-    <div>
-      <h1> Meu nome: {props.nome} </h1>
-      <h1> Meu cargo: {props.cargo} </h1>
-      <h1> Minha idade: {props.idade} </h1>      
-    </div>
-  )
+class Equipe extends Component {
+  render() {
+    return (
+      <div>
+        <Sobre nome = {this.props.nome} cargo={this.props.cargo} 
+               idade={this.props.idade} />
+        <Social goo={this.props.google} />
+      <hr/>
+      </div>
+    )
+  }
 }
 
 const Social = (props) => {
-  return (
+  return ( 
     <div>
-      <a href= {props.goo}>Google </a>
-      <a href= {props.face}>Facebook </a>
+      <a href={props.goo} > Google.com </a>
     </div>
   )
 }
+
+class Sobre extends Component {
+  render () {
+    return (
+      <div>
+        <h2> Olá sou o (a) {this.props.nome} </h2>
+        <h3> Trabalho como {this.props.cargo} </h3>
+        <h4> Minha idade é {this.props.idade} nos. </h4>        
+      </div>
+    )
+  }
+}
+
 
 
 function app() {
   return (
-    <div>      
-      <Equipe nome="john" cargo="Front-end" idade="48" google="https://google.com"
-      facebook="https://facebook.com" />
-
-      <Equipe nome="isabel" cargo="Back-end" idade="51" google="https://google.com"
-      facebook="https://facebook.com" />
+    <div>
+      <h1>Conheça nossa equipe</h1>
+      <Equipe nome= "John" cargo="Programador Front-end" idade="48" google="https://google.com" />
+      <Equipe nome= "Johrdan" cargo="Programador Full-stack" idade="28" google="https://google.com" />
     </div>
   )
 }
