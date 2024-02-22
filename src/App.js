95026837a32c663a5731073890b0ac6d68f2eab1
johntec-ads <johntec.ads@gmail.com>
2024-02-21 21:08:16 -0300

    react class

--- src/App.js
+++ src/App.js
@@ -11,49 +11,22 @@ const Variável = (props) => {
   )
 }  */
 
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 class Equipe extends Component {
   render() {
     return (
-      <div>
-        <Sobre nome = {this.props.nome} cargo={this.props.cargo} 
-               idade={this.props.idade} />
-        <Social goo={this.props.google} />
-      <hr/>
-      </div>
-    )
-  }
-}
-
-const Social = (props) => {
-  return ( 
-    <div>
-      <a href={props.goo} > Google.com </a>
-    </div>
-  )
-}
-
-class Sobre extends Component {
-  render () {
-    return (
-      <div>
-        <h2> Olá sou o (a) {this.props.nome} </h2>
-        <h3> Trabalho como {this.props.cargo} </h3>
-        <h4> Minha idade é {this.props.idade} nos. </h4>        
-      </div>
+      <h1> Nome : {this.props.nome} </h1>
     )
   }
 }
 
 
-
-function app() {
+function app () {
   return (
     <div>
-      <h1>Conheça nossa equipe</h1>
-      <Equipe nome= "John" cargo="Programador Front-end" idade="48" google="https://google.com" />
-      <Equipe nome= "Johrdan" cargo="Programador Full-stack" idade="28" google="https://google.com" />
+      <h2> Conheça nossa equipe :) </h2>
+      <Equipe nome="John" />
     </div>
   )
 }
