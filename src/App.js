0c5a95c6e006a4dd8b33bc227cfc11292fa8ccd2
johntec-ads <johntec.ads@gmail.com>
2024-02-24 09:26:32 -0300

    upReactState

--- src/App.js
+++ src/App.js
@@ -3,31 +3,35 @@ import React, { Component } from "react";
 class App extends Component {
 
   constructor(props) {
-
     super(props);
     this.state = {
-      nome: "john",
-      contador: 0
+      nome: "John",
+      contador: "0"
     }
+    this.contar = this.contar.bind(this)
+
   }
 
-  aumentar ( ) {
-    //Metodo que atualiza o valor da state.
-    this.setState( {contador: 1} )
-  }
+  contar() {
+    let state = this.state;
+    state.contador += 1;
+    this.setState(state);
 
+  }
 
 
   render() {
     return (
       <div>
+        <h1> {this.state.nome} </h1>
 
-        <h1> <button onClick={this.aumentar} >+</button> 
-          {this.state.contador} 
-        <button>-</button> </h1>
-
+        <h1> <button onClick={this.contar}>+</button>
+             {this.state.contador}
+             <button>-</button> 
+        </h1>
       </div>
     )
+
   }
 }
 
