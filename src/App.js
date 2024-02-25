0e06c6917e79922853200760ea3e46eb5b0498d4
johntec-ads <johntec.ads@gmail.com>
2024-02-25 09:20:38 -0300

    valorInput

--- src/App.js
+++ src/App.js
@@ -2,57 +2,57 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
+  constructor( props ) {
+    super( props );
     this.state = {
       nome: "",
       contador: 0
     };
     /* bind faz com que a function seja reconhecida
     no escopo global */
-    this.contar    = this.contar.bind(this);
-    this.descontar = this.descontar.bind(this);
-    this.rename    = this.rename.bind(this);
-
-
+    this.contar = this.contar.bind( this );
+    this.descontar = this.descontar.bind( this );
+    this.rename = this.rename.bind( this );
   }
 
-  
 
-  contar () {  
-    
+
+  contar () {
+
     let soma = this.state;
     soma.contador += 1;
     this.setState( soma )
   }
 
-  descontar ( ) {
+  descontar () {
     let desconto = this.state;
     desconto.contador -= 1;
-    this.setState(desconto);
+    this.setState( desconto );
 
   }
 
-  rename ( ) {
+  rename () {
     let newname = this.state;
-    newname.nome = document.getElementById('nome').value ;
-    this.setState(newname)
+    newname.nome = document.getElementById( 'nome' ).value;
+    this.setState( newname )
 
   }
 
-  render() {
+  render () {
 
     return (
       <div>
         <h1>
-          Nome: <input id='nome'></input> {this.state.nome} 
-                  <button onClick={this.rename} >Enter</button> 
-          </h1>
-          <h2>Novo nome: {this.state.nome} </h2>
+          Nome: <input id='nome'></input> { this.state.nome }
+          <button onClick={ this.rename } >Enter</button>
+          
+        </h1>
 
-        <h1> <button onClick={this.contar}>+</button> 
-                {this.state.contador} 
-             <button onClick= {this.descontar}>-</button>
+        {/* <h2>Novo nome: {this.state.nome} </h2> */ }
+
+        <h1> <button onClick={ this.contar }>+</button>
+          { this.state.contador }
+          <button onClick={ this.descontar }>-</button>
         </h1>
 
       </div>
