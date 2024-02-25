50314876ab8841ac08b2bac46b286c4808cca5e9
johntec-ads <johntec.ads@gmail.com>
2024-02-25 06:50:33 -0300

    react_class_state

--- src/App.js
+++ src/App.js
@@ -1,40 +1,35 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      nome: "John",
-      contador: "0"
-    }
-    this.contar = this.contar.bind(this)
+      nome: "john marcelo",
+      contador: 0
+    };
+    
+    this.contar = this.contar.bind(this);
 
   }
 
-  contar() {
-    let state = this.state;
-    state.contador += 1;
-    this.setState(state);
+  
 
+  contar () {
+    this.state.contador +=
+
+    this.setState( {contador} )
   }
-
 
   render() {
+
     return (
       <div>
         <h1> {this.state.nome} </h1>
-
-        <h1> <button onClick={this.contar}>+</button>
-             {this.state.contador}
-             <button>-</button> 
-        </h1>
+        <h1> <button onClick={this.contar} >+</button> {this.state.contador} <button>-</button> </h1>
       </div>
     )
-
   }
 }
 
-
-
-export default App
+export default App;
