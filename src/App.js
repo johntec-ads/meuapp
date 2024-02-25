1f9c2791989503855009c879831a7d088ba12d3d
johntec-ads <johntec.ads@gmail.com>
2024-02-25 09:08:47 -0300

    react_state_bind_byid

--- src/App.js
+++ src/App.js
@@ -5,28 +5,56 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      nome: "john marcelo",
+      nome: "",
       contador: 0
     };
-    
-    this.contar = this.contar.bind(this);
+    /* bind faz com que a function seja reconhecida
+    no escopo global */
+    this.contar    = this.contar.bind(this);
+    this.descontar = this.descontar.bind(this);
+    this.rename    = this.rename.bind(this);
+
 
   }
 
   
 
-  contar () {
-    this.state.contador +=
+  contar () {  
+    
+    let soma = this.state;
+    soma.contador += 1;
+    this.setState( soma )
+  }
 
-    this.setState( {contador} )
+  descontar ( ) {
+    let desconto = this.state;
+    desconto.contador -= 1;
+    this.setState(desconto);
+
+  }
+
+  rename ( ) {
+    let newname = this.state;
+    newname.nome = document.getElementById('nome').value ;
+    this.setState(newname)
+
   }
 
   render() {
 
     return (
       <div>
-        <h1> {this.state.nome} </h1>
-        <h1> <button onClick={this.contar} >+</button> {this.state.contador} <button>-</button> </h1>
+        <h1>
+          Nome: <input id='nome'></input> {this.state.nome} 
+                  <button onClick={this.rename} >Enter</button> 
+          </h1>
+          <h2>Novo nome: {this.state.nome} </h2>
+
+        <h1> <button onClick={this.contar}>+</button> 
+                {this.state.contador} 
+             <button onClick= {this.descontar}>-</button>
+        </h1>
+
       </div>
     )
   }
