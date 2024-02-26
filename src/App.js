b58f7bab236d8cc6508c2dd1349162b8702d6a29
johntec-ads <johntec.ads@gmail.com>
2024-02-25 21:17:30 -0300

    componentCiclosDeVida

--- src/App.js
+++ src/App.js
@@ -1,63 +1,48 @@
-import React, { Component } from 'react';
+import React, {Component} from "react";
 
 class App extends Component {
 
-  constructor( props ) {
-    super( props );
+  /* O contructor é o primeiro ciclo de vida */
+  constructor (props) {
+    super(props);
     this.state = {
-      nome: "",
-      contador: 0
-    };
-    /* bind faz com que a function seja reconhecida
-    no escopo global */
-    this.contar = this.contar.bind( this );
-    this.descontar = this.descontar.bind( this );
-    this.rename = this.rename.bind( this );
+      hora: "00:00:00"
+    }
+
   }
+  /* Ciclo de vida */
+  componentDidMount() {
 
-
-
-  contar () {
-
-    let soma = this.state;
-    soma.contador += 1;
-    this.setState( soma )
-  }
-
-  descontar () {
-    let desconto = this.state;
-    desconto.contador -= 1;
-    this.setState( desconto );
+    setInterval( () => {
+      this.setState( { hora: new Date().toLocaleTimeString() } )
+      
+    }, 1000 );
 
   }
 
-  rename () {
-    let newname = this.state;
-    newname.nome = document.getElementById( 'nome' ).value;
-    this.setState( newname )
-
+  /*Ciclo de vida */
+  componentDidUpdate() {
+    console.log("Atualizou!!!")
   }
+
+  shouldComponentUpdate() {
+    
+  }
+
+
 
   render () {
-
     return (
-      <div>
-        <h1>
-          Nome: <input id='nome'></input> { this.state.nome }
-          <button onClick={ this.rename } >Enter</button>
-          
-        </h1>
+    <div>
 
-        {/* <h2>Novo nome: {this.state.nome} </h2> */ }
-
-        <h1> <button onClick={ this.contar }>+</button>
-          { this.state.contador }
-          <button onClick={ this.descontar }>-</button>
-        </h1>
-
-      </div>
+      <h1> Relogio </h1>
+      <h2> {this.state.hora} </h2>
+    </div>      
     )
   }
 }
+
+
+
 
 export default App;
