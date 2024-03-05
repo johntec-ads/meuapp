115db3dc480bc2eb5ee951f86a1fe8261100615f
johntec-ads <johntec.ads@gmail.com>
2024-03-05 07:01:48 -0300

    up aula

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, {Component} from 'react';
-import Membro from './components/Membro/Membro';
+import React, { Component } from 'react';
+import Membro from './components/Membro';
 
 class App extends Component {
 
-  render() {
-    return(
+  render () {
+    return (
       <div>
-        <Membro/>
+
+        <Membro nome="Visitante" />        
 
       </div>
     );
   }
 }
-
-
-
 
 export default App;
