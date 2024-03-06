a28553c5083d08f057f6af279e978476cc2113e5
johntec-ads <johntec.ads@gmail.com>
2024-03-06 06:50:36 -0300

    up aula

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
-import Membro from './components/Membro';
 
 class App extends Component {
+
+  constructor( props ) {
+    super( props );
+    this.state = {
+      status: false
+    }
+    this.trocar = this.trocar.bind(this);
+  }
+
+  trocar() {
+    this.setState( {status: true} )
+  }
+
+
 
   render () {
     return (
       <div>
+        <button onClick={this.trocar}>Trocar bolean</button>
+        { this.state.status ?
+          <div>
+            <h2>Bem-vindo ao sistema</h2>
+          </div>:
+          <div>
+            <h2>Olá visitante, faça o login!</h2>
+          </div>
+        }
 
-        <Membro nome="Visitante" />        
+
 
       </div>
-    );
+    )
   }
 }
+
 
 export default App;
