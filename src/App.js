78707555a844e8ba67ffed534ddd24499eb52d3a
John Marcelo <johntec.ads@gmail.com>
2024-03-10 16:51:20 -0300

    subirAula

--- src/App.js
+++ src/App.js
@@ -4,23 +4,35 @@ class App extends Component {
   constructor( props ) {
     super( props );
     this.state = {
-      status: true,
+      status: false
     }
+
+    this.entrar = this.entrar.bind(this);
+    this.sair = this.sair.bind(this);
+  }
+
+  entrar() {
+    this.setState( {status: false} )
+  }
+  sair() {
+    this.setState( {status: true} )
+
   }
 
   render () {
     return (
       <div>
         { this.state.status ?
+          <div>            
+            <h1> Olá,bem-vindo ao sistema.</h1>
+            <button onClick={this.entrar} >Off system</button>
+          </div>
+          :
           <div>
-            
-          </div>  
-      
-      
-      
-      
-      }
-
+            <h1>System off</h1>
+            <button onClick={this.sair} >Entrar sytem</button>
+          </div>
+        }
       </div>
     )
   }
