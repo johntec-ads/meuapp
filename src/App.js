ab02e10e706439d7ef91fe448d313549af1df1dc
johntec-ads <johntec.ads@gmail.com>
2024-03-19 06:47:51 -0300

    teste codigos

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
-import React, { Component } from "react";
-import Feed from './components/feed/index'
+import React, { Component } from 'react';
+class App extends Component {///
 
-class App extends Component {
   constructor( props ) {
     super( props );
     this.state = {
       feed: [
-        {
-          id: 1, nome: 'John', curtidas: 13, comentarios: 454
-        },
-        {
-          id: 2, nome: 'Isabel', curtidas: 53, comentarios: 1454
-        },
-        {
-          id: 3, nome: 'Johrdan', curtidas: 513, comentarios: 4454
-        }
+        { id: 1, username: 'John', classe: '', compras: '10' },
+        { id: 1, username: 'Isabel', classe: '', compras: '20' },
+        { id: 1, username: 'Johrdan', classe: '', compras: '30' },
+        { id: 1, username: 'Pacato', classe: '', compras: '40' },
       ]
+
     }
   }
-
 
   render () {
     return (
       <div>
-        { this.state.feed.map( ( item ) => (
-          <Feed
-            key={ item.id }
-            nome={ item.nome }
-            curtidas={ item.curtidas }
-            comentarios={ item.comentarios }
-          />
-        ) ) }
-      </div>
+        { this.state.feed.map( ( item ) => {
+          return (
+            <div key={ item.id }>
+              <h2> { item.username } </h2>
 
+              <a>
+                { item.compras > 0 && item.compras <=10  ? item.classe + 'sem classificação':
+                  item.compras < 30 ? item.classe + 'Bronze':
+                  item.classe + 'Ouro'}
+              </a>
+
+
+
+            </div>
+
+          )
+
+        } ) }
+      </div>
     )
   }
 }
