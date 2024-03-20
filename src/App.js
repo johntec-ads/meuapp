acb9b1696e94146d151dafa910f7156130830f2d
johntec-ads <johntec.ads@gmail.com>
2024-03-20 06:59:03 -0300

    listaCondicionalBotton

--- src/App.js
+++ src/App.js
@@ -5,30 +5,44 @@ class App extends Component {///
     super( props );
     this.state = {
       feed: [
-        { id: 1, username: 'John', classe: '', compras: '10' },
+        { id: 1, username: 'John', classe: '', compras: '30' },
         { id: 1, username: 'Isabel', classe: '', compras: '20' },
         { id: 1, username: 'Johrdan', classe: '', compras: '30' },
         { id: 1, username: 'Pacato', classe: '', compras: '40' },
       ]
 
     }
+    this.somar = this.somar.bind( this );
+    this.subtrair = this.subtrair.bind( this );
   }
+
+  somar = ( index ) => {
+    const newfeed = [ ...this.state.feed ];
+    newfeed[ index ].compras = String( Number( newfeed[ index ].compras ) ) + 1
+  };
+
+  subtrair = ( index ) => {
+    const newfeed = [ ...this.state.feed ];
+    newfeed[ index ].compras = String( Number( newfeed[ index ].comprar ) ) - 1
+  };
 
   render () {
     return (
       <div>
-        { this.state.feed.map( ( item ) => {
+        { this.state.feed.map( ( item, index ) => {
           return (
+
             <div key={ item.id }>
-              <h2> { item.username } </h2>
+              <h2> <button onClick={ () => this.somar( index ) }> + </button> { item.username } <button onClick={ () => this.subtrair( index ) }> - </button>  </h2 >
 
               <a>
-                { item.compras > 0 && item.compras <=10  ? item.classe + 'sem classificação':
-                  item.compras < 30 ? item.classe + 'Bronze':
-                  item.classe + 'Ouro'}
+                Classificação : { item.compras > 0 && item.compras <= 10 ? item.classe + 'Inciante' :
+                  item.compras < 20 ? item.classe + 'Bronze' :
+                    item.compras < 30 ? item.classe + 'Prata' :
+                      item.classe + 'Ouro' } /
               </a>
 
-
+              <a> Compras: { item.compras } </a>
 
             </div>
 
