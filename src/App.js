3364eda2159574c48be580aaaf6e20b21fcef974
John Marcelo <johntec.ads@gmail.com>
2024-04-09 15:21:21 -0300

    reactDoZero

--- src/App.js
+++ src/App.js
@@ -1,52 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react' ;
+
 class App extends Component {
 
-  constructor( props ) {
-    super( props );
-
-    this.state = {
-
-      status: [
-        {
-          id: 1,
-          nome: ' cliente 01 ',
-          classificacao: 'ouro'
-
-        },
-        {
-          id: 2,
-          nome: ' cliente 02 ',
-          classificacao: 'prata'
-
-        },
-        {
-          id: 3,
-          nome: ' cliente 03 ',
-          classificacao: 'bronze'
-
-        },
-
-      ]
-    }
-  }
-
-  render () {
+  render {
     return (
       <div>
-        {this.state.status.map ((item) => { 
-          return (
-            <div key={item.id}>
-              <h3> {item.nome} / </h3>
-              <p> {item.classificacao} </p>
-             </div> 
-
-          )
-        })}
-
+        <h1>teste</h1>
       </div>
     )
   }
+
+
+
 }
 
 
-export default App;
+
+export default App ;
