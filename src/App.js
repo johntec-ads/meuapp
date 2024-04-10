9a95e7fcc16db3fbc1df90760bb2fa1f0a8d1548
johntec-ads <johntec.ads@gmail.com>
2024-04-10 06:52:22 -0300

    upaula

--- src/App.js
+++ src/App.js
@@ -3,27 +3,20 @@ import React from 'react';
 const Cursos = (props) => {
   return (
     <div>
+      <h1> Bem-vindo ao Curso</h1>
       <h2> {props.curso} </h2>
-      <h3> Bem-vindo  </h3>
+      <h2> {props.nome } </h2>
     </div>
   );
 }
 
 
-const Equipe = (props) => {
-  return (
-    <div>     
-     {props.nome}
-    </div>
-  )
-}
 
 
 function App () {  
   return (
     <div>      
-      <Cursos curso = 'React_JS' />
-      <Equipe nome='John'  />      
+      <Cursos   curso = 'React_JS' nome='Isabel' />         
       
     </div>
   )
