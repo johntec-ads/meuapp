3c608f413e2b52ed4975ec969ee02c8ffc5f99c3
John Marcelo <johntec.ads@gmail.com>
2024-04-15 21:53:57 -0300

    up recapitulando

--- src/App.js
+++ src/App.js
@@ -1,36 +1,55 @@
-import React, {Component} from 'react';
+import React from 'react'
 
-class Equipe extends Component {
-  render () {
-    return (
-      <div>
-        teste
-      </div>
-    )
-  }
+//Component para boas vindas
+//Criado com area function
+const Equipe = ( props ) => {
+  return (
+    <div>
+      Nossa equipe é...
+      <Sobre nome={ props.nome } idade={ props.idade } profissa={ props.profissao } />
+      <Social/>
+      <hr></hr>
+
+    </div>
+  )
+}
+
+const Sobre = ( props ) => {
+  return (
+    <div>
+      <h4>Nome: { props.nome }</h4>
+      <a> Idade: { props.idade } /  </a>
+      <a> cargo: { props.profissa } </a>
+      
+    </div>
+  )
+}
+
+const Social = () => {
+  return (
+    <div>
+      <a> Facebook </a>      
+      <a> Youtube  </a>      
+      <a> Whatsapp </a>      
+      
+    </div>
+  )
 }
 
 
 
-class App extends Component { 
-  
-  constructor (props) {
-    super(props);
-    this.state = {
 
-    }
-  }  
-  
-  render() {
-    return (
-      <div>
-        <Equipe/>
-      </div>
-    )
-  }
+
+
+//Component principal
+function App () {
+  return (
+    <div>
+      <h2> Conheça nossa equipe: </h2>
+      <Equipe nome='Isabel' idade='50' profissao='Agente Financeiro' />
+      <Equipe nome='John' idade='48' profissao='Front-end' />
+    </div>
+  )
 }
 
-
-
-
-export default App ;
+export default App;
