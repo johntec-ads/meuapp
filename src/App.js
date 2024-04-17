f25a113e7491105238f1147d576ef025c29184b2
johntec-ads <johntec.ads@gmail.com>
2024-04-17 06:53:11 -0300

    recaptulando

--- src/App.js
+++ src/App.js
@@ -1,53 +1,44 @@
 import React from 'react'
 
-//Component para boas vindas
-//Criado com area function
+//component Equipe
 const Equipe = ( props ) => {
   return (
     <div>
-      Nossa equipe é...
-      <Sobre nome={ props.nome } idade={ props.idade } profissa={ props.profissao } />
-      <Social/>
-      <hr></hr>
+      <Sobre nome={ props.nome } idade= {props.idade} cargo= {props.cargo} />
+
+      <Social face={props.rede1} />
 
     </div>
   )
 }
 
-const Sobre = ( props ) => {
+const Sobre = ( props ) => {//component
   return (
     <div>
-      <h4>Nome: { props.nome }</h4>
-      <a> Idade: { props.idade } /  </a>
-      <a> cargo: { props.profissa } </a>
-      
+      <h2> Nome : { props.nome } </h2>
+      <a> Idade: {props.idade} </a> /
+      <a> Cargo: {props.cargo} </a>    
     </div>
   )
 }
 
-const Social = () => {
+const Social = (props) => {
   return (
     <div>
-      <a> Facebook </a>      
-      <a> Youtube  </a>      
-      <a> Whatsapp </a>      
-      
+    <a> {props.face} </a>
     </div>
   )
 }
 
 
-
-
-
-
-//Component principal
-function App () {
+//component principal
+const App = () => {
   return (
     <div>
-      <h2> Conheça nossa equipe: </h2>
-      <Equipe nome='Isabel' idade='50' profissao='Agente Financeiro' />
-      <Equipe nome='John' idade='48' profissao='Front-end' />
+       {/* Valores do component */ }
+      <Equipe nome='John' idade='48' cargo='Front-end' rede1 = 'http://facebook.com'/>
+      <Equipe nome='Isabel' idade='50' cargo='Enginer IA' />
+      <Equipe nome='Johrdan' idade='28' cargo='Cientista de dados' />        
     </div>
   )
 }
