d081af1aca34ed573e1f74e95b38d739a3715212
johntec-ads <johntec.ads@gmail.com>
2024-04-25 08:41:14 -0300

    react_list

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react' ;
-import Membro from './components/Membro'
 
 class App extends Component {
 
+  constructor (props) {
+    super (props);
+    this.state = {
+
+      feed : [
+        { id: 1 , nome: 'John', curtidas: 10, comentarios: 3 },
+        { id: 2 , nome: 'Johrdan', curtidas: 20, comentarios: 6 },
+        { id: 3 , nome: 'Isabel', curtidas: 16, comentarios: 7 },
+        { id: 4 , nome: 'Trator', curtidas: 100, comentarios: 73 },
+        { id: 5 , nome: 'Pacato', curtidas: 1000, comentarios: 83 },
+      ]      
+      
+    }
+
+  }
+
+
   render() {
-
     return(
+      <div> 
 
-      <div>
+        { this.state.feed.map( (item) => {
+          
+          return (
+            <div key={item.id}>
 
-        <Membro nome='Visitante' />
-        
+              <h3> {item.nome} </h3>
+              <a> Curtidas:  {item.curtidas} / </a>
+              <a> Comentários {item.comentarios} </a>
+
+            </div>
+          )
+        } ) }
+
       </div>
     )
   }
 }
-
 
 export default App ;
