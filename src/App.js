f35d3f6c454e8ddf7070044ae3adaab97cc321fb
johntec-ads <johntec.ads@gmail.com>
2024-04-29 07:16:52 -0300

    react_list_past_condition

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
-import React, { Component } from 'react' ;
+import React, { Component } from 'react';
+import Feed from './components/Feed' ;
 
 class App extends Component {
 
-  constructor (props) {
-    super (props);
+  constructor( props ) {
+    super( props );
     this.state = {
 
-      feed : [
-        { id: 1 , nome: 'John', curtidas: 10, comentarios: 3 },
-        { id: 2 , nome: 'Johrdan', curtidas: 20, comentarios: 6 },
-        { id: 3 , nome: 'Isabel', curtidas: 16, comentarios: 7 },
-        { id: 4 , nome: 'Trator', curtidas: 100, comentarios: 73 },
-        { id: 5 , nome: 'Pacato', curtidas: 1000, comentarios: 83 },
-      ]      
-      
+      feed: [
+        { id: 1, nome: 'John', curtidas: 1, comentarios: 1 },
+        { id: 2, nome: 'Johrdan', curtidas: 20, comentarios: 6 },
+        { id: 3, nome: 'Isabel', curtidas: 16, comentarios: 7 },
+        { id: 4, nome: 'Trator', curtidas: 100, comentarios: 73 },
+        { id: 5, nome: 'Pacato', curtidas: 1000, comentarios: 83 },
+      ]
+
     }
 
   }
 
 
-  render() {
-    return(
-      <div> 
+  render () {
+    return (
+      <div>
 
-        { this.state.feed.map( (item) => {
-          
+        { this.state.feed.map( ( item ) => {
+
           return (
-            <div key={item.id}>
-
-              <h3> {item.nome} </h3>
-              <a> Curtidas:  {item.curtidas} / </a>
-              <a> Comentários {item.comentarios} </a>
-
-            </div>
-          )
+            <Feed
+              id={ item.id }
+              nome={ item.nome }
+              curtidas={ item.curtidas }
+              comentarios={ item.comentarios }
+            />
+          );
         } ) }
 
       </div>
@@ -41,4 +41,4 @@ class App extends Component {
   }
 }
 
-export default App ;
+export default App;
