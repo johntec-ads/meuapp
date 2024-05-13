c47074006a303759bcf3eaa9e8296a548040d477
johntec-ads <johntec.ads@gmail.com>
2024-05-13 06:58:54 -0300

    input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React, { Component } from 'react' ;
+import React, { Component } from 'react';
 
 class App extends Component {
 
-  constructor (props) {
-    super(props);
+  constructor( props ) {
+    super( props );
     this.state = {
-      
+      email: '',
+      senha: '',
+      sexo: 'masculino'
     }
+    this.mudarEmail = this.mudarEmail.bind( this );
 
   }
-  render() {
+
+  mudarEmail ( e ) {
+    //e -> props / target -> input apontado / value -> digitado 
+    let valorDigitado = e.target.value;// recebe o input
+    this.setState( { email: valorDigitado } );//recebe o novo valor
+
+  }
+
+  render () {
 
     return (
+      <div>
+        <h2> Loguin </h2>
+        Senha
+        <input type='text'
+          name='senha'
+          value={ this.state.senha }
+          onChange={ ( e ) => this.setState( { senha: e.target.value } ) }
+        />
+        <br></br>
+
+        E-mail
+        <input type='email'
+          name='email'
+          value={ this.state.email }
+          onChange={ this.mudarEmail }
+        />
+        <br></br>
+
+        Sexo :
+        <select>
+          <option>Masculino</option>
+          <option>Feminino</option>
+        </select>
 
         <div>
-          E-mail <br></br>
-          <input type='email' name='email' value= 'teste@teste.com' />
-
+          <h3> { this.state.senha } </h3>
+          <h3> { this.state.email } </h3>
         </div>
+
+
+      </div>
+
     )
   }
 }
 
 
-export default App ;
+export default App;
