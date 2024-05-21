2a029265813d72b37dc1c6441eb7e52454a52e8f
John Marcelo <johntec.ads@gmail.com>
2024-05-21 08:02:24 -0300

    upReactForms

--- src/App.js
+++ src/App.js
@@ -19,84 +19,26 @@
           </select>
   
 */
+import React, { Component } from 'react' ;
 
-import React, { Component } from 'react';
 class App extends Component {
-
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      form: {
-        nome: '',
-        email: '',
-        senha: '',
-        sexo: ''
-      }   
-    }    
-
-    this.dadosForm = this.dadosForm.bind(this)
-    
+      teste: ' teste'
+    }
   }
 
-  dadosForm(e) {
-    let form = this.state.form;
-    form[e.target.name] = e.target.value;
-    this.setState({form: form});
-
-  }
-
-    
   render() {
     return(
       <div>
-          <h1>Loguin</h1>  
         
-          <label> Nome: </label>
-          <input
-                  type='text'
-                  name='nome'
-                  value={this.state.form.nome}
-                  onChange={this.dadosForm}
-           />
-          <br></br>
-
-          <label> Email: </label>
-          <input
-            type='email'
-            name='email'
-            value={this.state.form.email}
-            onChange={ this.dadosForm }
-          />
-          <br></br>
-          <label> Senha: </label>
-          <input
-            type='password'
-            name='senha'
-            value={this.state.form.senha}
-            onChange={this.dadosForm}
-          />
-          <br></br>
-
-          <label>Sexo</label>
-          <select name='sexo' value={this.state.form.sexo} onChange={this.dadosForm} >
-            <option>Masculino</option>
-            <option>Feminino</option>
-          </select>
-
-          <br></br>              
-      
-
-        <div>
-          <h3> {this.state.form.nome} </h3>
-          <h3> {this.state.form.email} </h3>
-          <h3> {this.state.form.senha} </h3>
-          <h3> {this.state.form.sexo} </h3>
-        </div>
-
       </div>
+    
     )
   }
 }
 
 
 export default App ;
+
