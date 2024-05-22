13163396315d581e16cc24e27dcc6c491720f453
John Marcelo <johntec.ads@gmail.com>
2024-05-22 15:53:23 -0300

    reactForm

--- src/App.js
+++ src/App.js
@@ -21,55 +21,53 @@
           </select>
   
 */
-import React, { Component } from 'react' ;
-
+import React, { Component } from 'react';
 class App extends Component {
-  constructor(props) {
-    super(props)
+  constructor( props ) {
+    super( props );
     this.state = {
       form: {
-        nome: 'john',
+        nome: 'John',
         email: 'john@gmail.com',
         senha: '123123',
         sexo: 'masculino'
       }
+
     }
-
-    this.dadosForm = this.dadosForm.bind(this) ;
+    this.dadosForm = this.dadosForm.bind( this );
+  }
+  dadosForm ( e ) {
+    let form = this.state.form
 
   }
 
-  dadosForm (e) {
 
-    let form = this.state.form ;
-    form[e.target.name] = e.target.value;
-    this.setState( {form: form} )
-  }
-
-  render() {
-    return(
+  render () {
+    return (
       <div>
-        <label>Nome : </label>
-        <input type='text'      name='nome'  value={this.state.form.nome}  onChange={this.dadosForm} ></input><br></br>
-        <label>E-mail : </label>
-        <input type='email'     name='email' value={this.state.form.email} onChange={this.dadosForm} ></input><br></br>
-        <label>Senha : </label>
-        <input type='password'  name='senha' value={this.state.form.senha} onChange={this.dadosForm} ></input><br></br>
-        <label>Sexo : </label>
-        <select>
-          <option>Masculino</option>
-          <option>Feminino</option>
+        <labe>Nome : </labe>
+        <input type='text' name='nome'
+          value={ this.state.form.nome }
+          onChange={ this.dadosForm }>
+        </input>
+        <br></br>
+        <labe>E-mail : </labe>
+        <input  type='email' name='email'
+                value={this.state.form.email}
+                onChange={this.dadosForm}>                  
+       </input>
+       <br></br>
+       <labe>Senha : </labe>
+       <input type='password' name='senha'
+              value={this.state.form.senha}
+              onChange={this.dadosForm}>
+       </input>
+       
 
-        </select>
-                
 
-              
       </div>
-    
     )
   }
 }
 
-
-export default App ;
-
+export default App;
